9401109f1d0ab267c4270da38ce2beed44190b09
AayushPrakash24 <aayush.prakash@outlook.com>
2025-06-14 22:55:12 -0400

    trie solution

--- Day-19/word-break.py
+++ Day-19/word-break.py
@@ -13,4 +13,51 @@ class Solution:
 
         return dp[-1]
                         
+
+
+class Trie:
+    def __init__(self):
+        self.trie = {}
+    
+    def insert(self, s):
+        d = self.trie
+
+        for c in s:
+            if c not in d:
+                d[c] = {}
+            
+            d = d[c]
+        d['.'] = '.'
+    
+    def search(self,word):
+        d = self.trie
+
+        for c in word:
+            if c not in d:
+                return False
+            d = d[c]
         
+        return '.' in d
+    
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        root = Trie()
+        max_len = 0
+    
+        for word in wordDict:
+            root.insert(word)
+            max_len = max(max_len, len(word))
+
+        dp = [False] * (n+1)
+        dp[0] = True
+
+        for i in range(n):
+            if dp[i]:
+                curr = root
+                
+                for j in range(1, min(n-i,max_len)+1):
+                    if curr.search(s[i:i+j]):
+                        dp[i+j] = True
+        return dp[n]
